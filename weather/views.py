40a0862daf27eade0a65d6f38da9c719a3f7bb0f
Vipul Sharma <33789550+vipsh18@users.noreply.github.com>
2022-03-07 18:08:33 +0530

    Update views

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from datetime import datetime
 import json
 import urllib.request
 
@@ -14,14 +15,21 @@ def index(request):
         ).read()
         json_data = json.loads(res)
 
+        main_json_data = json_data["main"]
         data = {
-            "country_code": str(json_data["sys"]["country"]),
-            "coordinate": str(json_data["coord"]["lon"])
-            + " "
-            + str(json_data["coord"]["lat"]),
-            "temp": str(json_data["main"]["temp"]) + "K",
-            "pressure": str(json_data["main"]["pressure"]),
-            "humidity": str(json_data["main"]["humidity"]),
+            "country_code": f"{json_data['sys']['country']}",
+            "coordinate": f"{json_data['coord']['lon']}, {json_data['coord']['lat']}",
+            "celsiusTemp": f"{main_json_data['temp'] - 273.15:.1f} °C",
+            "kelvinTemp": f"({main_json_data['temp']} K)",
+            "feelsLikeCelsius": f"{main_json_data['feels_like'] - 273.15:.1f} °C",
+            "feelsLikeKelvin": f"({main_json_data['feels_like']} K)",
+            "pressure": f"{main_json_data['pressure']} hPa",
+            "humidity": f"{main_json_data['humidity']} %",
+            "visibility": f"{json_data['visibility']} m",
+            "windSpeed": f"{json_data['wind']['speed']} mph",
+            "sunrise": f"{datetime.fromtimestamp(json_data['sys']['sunrise'])}",
+            "sunset": f"{datetime.fromtimestamp(json_data['sys']['sunset'])}",
+            "description": f"{json_data['weather'][0]['description']}",
         }
     else:
         city = ""
