be1bac39bc241e520ab250c28105e081475fcbae
Vipul Sharma <33789550+vipsh18@users.noreply.github.com>
2022-03-11 18:56:09 +0530

    Add responsiveness

--- weather/views.py
+++ weather/views.py
@@ -8,29 +8,35 @@ import urllib.request
 def index(request):
     if request.method == "POST":
         city = request.POST["city"]
-        apiKey = "cb771e45ac79a4e8e2205c0ce66ff633"
+        apiKey = "f4d6a7640f463a5c53d68ba6d2e41448"
 
         res = urllib.request.urlopen(
             f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={apiKey}"
         ).read()
         json_data = json.loads(res)
+        cod = str(json_data["cod"])
 
-        main_json_data = json_data["main"]
-        data = {
-            "country_code": f"{json_data['sys']['country']}",
-            "coordinate": f"{json_data['coord']['lon']}, {json_data['coord']['lat']}",
-            "celsiusTemp": f"{main_json_data['temp'] - 273.15:.1f} °C",
-            "kelvinTemp": f"({main_json_data['temp']} K)",
-            "feelsLikeCelsius": f"{main_json_data['feels_like'] - 273.15:.1f} °C",
-            "feelsLikeKelvin": f"({main_json_data['feels_like']} K)",
-            "pressure": f"{main_json_data['pressure']} hPa",
-            "humidity": f"{main_json_data['humidity']} %",
-            "visibility": f"{json_data['visibility']} m",
-            "windSpeed": f"{json_data['wind']['speed']} mph",
-            "sunrise": f"{datetime.fromtimestamp(json_data['sys']['sunrise'])}",
-            "sunset": f"{datetime.fromtimestamp(json_data['sys']['sunset'])}",
-            "description": f"{json_data['weather'][0]['description']}",
-        }
+        if cod == "200":
+            main_json_data = json_data["main"]
+            data = {
+                "country_code": f"{json_data['sys']['country']}",
+                "coordinate": f"{json_data['coord']['lon']}, {json_data['coord']['lat']}",
+                "celsiusTemp": f"{main_json_data['temp'] - 273.15:.1f} °C",
+                "kelvinTemp": f"({main_json_data['temp']} K)",
+                "feelsLikeCelsius": f"{main_json_data['feels_like'] - 273.15:.1f} °C",
+                "feelsLikeKelvin": f"({main_json_data['feels_like']} K)",
+                "pressure": f"{main_json_data['pressure']} hPa",
+                "humidity": f"{main_json_data['humidity']}%",
+                "visibility": f"{json_data['visibility']} m",
+                "windSpeed": f"{json_data['wind']['speed']} mph",
+                "sunrise": f"{datetime.fromtimestamp(json_data['sys']['sunrise'])} GMT",
+                "sunset": f"{datetime.fromtimestamp(json_data['sys']['sunset'])} GMT",
+                "description": f"{json_data['weather'][0]['main']}",
+                "cityName": json_data["name"],
+                "cod": cod,
+            }
+        elif cod == "404":
+            data = {"cityName": json_data["cityName"], "cod": cod}
     else:
         city = ""
         data = {}
